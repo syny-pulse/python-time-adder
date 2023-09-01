ff6c77f56e54dc027c706e1b6eb2b859fadc765e
syny-pulse <sinaijose002@gmail.com>
2023-09-02 00:24:05 +0300

    more accurate

--- app.py
+++ app.py
@@ -42,11 +42,28 @@ def time_adding_machine():
         answer_hour -= 12
         time_half = 'PM'
 
+    if day == 1:
+        day_name = 'Monday'
+    elif day == 2:
+        day_name = 'Tuesday'
+    elif day == 3:
+        day_name = 'Wednesday'
+    elif day == 4:
+        day_name = 'Thursday'
+    elif day == 5:
+        day_name = 'Friday'
+    elif day == 6:
+        day_name = 'Saturday'
+    elif day == 7:
+        day_name = 'Sunday'
+    else:
+        day_name = day
+    
 
     if day > 1:
-        print(f"The answer is {answer_hour:02d}:{answer_minute:02d}:{answer_second:02d}:{time_half} on the {day} day")
+        print(f"The answer is {answer_hour:02d}:{answer_minute:02d}:{answer_second:02d}:{time_half} on {day_name} day {day}")
     else:
-        print(f"The answer is {answer_hour:02d}:{answer_minute:02d}:{answer_second:02d}:{time_half}")
+        print(f"The answer is {answer_hour:02d}:{answer_minute:02d}:{answer_second:02d}:{time_half}on {day_name}")
 
 # Call the function to run the time adding machine
 time_adding_machine()
